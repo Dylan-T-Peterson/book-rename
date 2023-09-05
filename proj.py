31b8828fd29242b13ca7eef66dc4a1c05f7937b9
Dylan-T-Peterson <dylantypeterson@gmail.com>
2023-09-05 12:22:23 -0800

    Started work on ISBN based renaming for epub files

--- proj.py
+++ proj.py
@@ -1,7 +1,12 @@
 import urllib.request as request
+import string
+from glob import iglob
+
+from ebooklib import epub
 
 
-def google_api_vol_req_generator(
+# def google_api_vol_req_generator(
+def test(
     api_key: str,
     intitle: str = "",
     inauthor: str = "",
@@ -18,7 +23,7 @@ def google_api_vol_req_generator(
         "inauthor:": f"{inauthor}",
         "inpublisher:": f"{inpublisher}",
         "subject:": f"{subject}",
-        "isbn:": f"{isbn}",
+        "isbn:": f"{isbn}",  # International Standard Book Number
         "lccn:": f"{lccn}",  # Library of Congress Control Number
         "oclc:": f"{oclc}",  # Online Computer Library Center Number
     }
@@ -47,9 +52,41 @@ def google_api_parser(api_req: str):
     pass
 
 
+def title_epub(file: str):
+    try:
+        book = epub.read_epub(file)
+    except Exception as e:
+        print(f"\n\n{file}\n{e} -- err\n\n")
+        return
+
+    identifier = book.get_metadata("DC", "identifier")
+    if "ISBN" in identifier[0][1].values():
+        isbn = identifier[0][0]
+        isbn = isbn.strip(
+            f"""{
+                    string.ascii_letters,
+                    string.punctuation,
+                    string.whitespace
+                    }""",
+        )
+        isbn_strip = isbn.replace("-", "")
+        if len(isbn_strip) in (10, 13):
+            print(file)
+            print(isbn, "\n")
+        # else:
+        #     print(file)
+        #     print(isbn, "- long", "\n")
+
+    else:
+        pass
+
+
 def main():
+    path = "books/"
     api_key = open("apikey.txt", "r").read()
-    google_api_vol_req_generator(api_key)
+
+    for file in iglob(f"{path}*.epub"):
+        title_epub(file)
 
 
 if __name__ == "__main__":
